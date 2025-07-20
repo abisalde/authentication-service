03b7916335035a9ffb785d0cf999e0a6749e452b
Isaiah Abiodun <isaiah.abiodun@outlook.com>
2025-07-20 14:12:44 +0100

    smoking let's go

--- internal/configs/config.go
+++ internal/configs/config.go
@@ -2,6 +2,7 @@ package configs
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
 	"strings"
@@ -78,8 +79,8 @@ func Load(env string) (*Config, error) {
 }
 
 func (c *Config) SQL_DSB() string {
-	// log.Printf("%s:%s@tcp(%s:%d)/%s?parseTime=true",
-	// 	c.DB.User, c.DB.Password, c.DB.Host, c.DB.Port, c.DB.Name)
+	log.Printf("%s:%s@tcp(%s:%d)/%s?parseTime=true",
+		c.DB.User, c.DB.Password, c.DB.Host, c.DB.Port, c.DB.Name)
 	return fmt.Sprintf(
 		"%s:%s@tcp(%s:%d)/%s?parseTime=true",
 		c.DB.User, c.DB.Password, c.DB.Host, 3306, c.DB.Name,
